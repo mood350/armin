e8be7242d85f2d54c1d011d0c12f530807800ca0
Prince <princiuso350@gmail.com>
2026-03-24 14:38:32 +0000

    modifying api url

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://armin-iu31.onrender.com/",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://armin-iu31.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
